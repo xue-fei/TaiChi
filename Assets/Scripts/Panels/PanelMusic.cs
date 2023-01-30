aef4825670c243e0c4cf4f2b8dce20d1ad02e7d1
xuefei <awnuxcvbn@163.com>
2023-01-30 11:30:11 +0800

    随机播放歌曲

--- Assets/Scripts/Panels/PanelMusic.cs
+++ Assets/Scripts/Panels/PanelMusic.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using TMPro;
@@ -13,23 +13,17 @@ public class PanelMusic : MonoBehaviour
     public Image image;
     public Button buttonRefresh;
     public TextMeshProUGUI textName;
-    string apiUrl = "https://api.uomg.com/api/rand.music?sort=%E7%83%AD%E6%AD%8C%E6%A6%9C&format=json";
+    public TextMeshProUGUI textSinger;
+    /// <summary>
+    /// https://api.wya6.cn/doc/NetEase_random.html
+    /// </summary>
+    string apiUrl = "https://api.wya6.cn/api/NetEase_random?return=json";
     string musicPath;
     AudioSource audioSource;
+    float pauseTime;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        ApiData apiData = new ApiData();
-        apiData.code = 1;
-        apiData.data.name = "aaaa";
-        apiData.data.url = "bbbb";
-        apiData.data.picurl = "cccc";
-        apiData.data.artistsname = "dddd";
-
-      string json =  JsonUtility.ToJson(apiData);
-        Log.Warning(json);
-
         musicPath = Application.persistentDataPath + "/Music";
         if (!Directory.Exists(musicPath))
         {
@@ -37,9 +31,36 @@ public class PanelMusic : MonoBehaviour
         }
         image = transform.Find("Image").GetComponent<Image>();
         buttonRefresh = transform.Find("ButtonRefresh").GetComponent<Button>();
-        textName = transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>();
+        textName = transform.Find("TextName").GetComponent<TextMeshProUGUI>();
+        textSinger = transform.Find("TextSinger").GetComponent<TextMeshProUGUI>();
         audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonRefresh.onClick.AddListener(() => StartCoroutine(RequestMusicUrl()));
         StartCoroutine(RequestMusicUrl());
+    }
+
+    private void OnEnable()
+    {
+        Log.Warning("OnEnable");
+        if (audioSource.clip != null)
+        {
+            audioSource.Play();
+            audioSource.time = pauseTime;
+        }
+    }
+
+    private void OnDisable()
+    {
+        Log.Warning("OnDisable");
+        if (audioSource.isPlaying)
+        {
+            audioSource.Pause();
+            pauseTime = audioSource.time;
+        }
     }
 
     private IEnumerator RequestMusicUrl()
@@ -56,26 +77,33 @@ public class PanelMusic : MonoBehaviour
             string musicUrl = "";
             try
             {
-                ApiData apiData = new ApiData();
-                JsonUtility.FromJsonOverwrite(uwr.downloadHandler.text, apiData);
                 Log.Warning(uwr.downloadHandler.text);
+                ApiData apiData = JsonConvert.DeserializeObject<ApiData>(uwr.downloadHandler.text);
+                if (apiData.code != 200)
+                {
+                    Log.Error("apiData.code:" + apiData.code);
+                    yield break;
+                }
+                textName.text = apiData.data.name;
+                textSinger.text = apiData.data.singer;
                 musicUrl = apiData.data.url;
-                Log.Warning(apiData.data.url);
-                Log.Warning(apiData.data.name);
-                Log.Warning(apiData.data.picurl);
-                ImgLoader.Instance.DownLoad(image, apiData.data.picurl, musicPath, apiData.data.name + ".jpg");
+                string coverName = apiData.data.singer + "-" + apiData.data.name;
+                ImgLoader.Instance.DownLoad(image, apiData.data.cover, musicPath, coverName);
+                string musicFilePath = musicPath + "/" + apiData.data.singer + "-" + apiData.data.name + ".mp3";
+                string lyricPath = musicPath + "/" + apiData.data.singer + "-" + apiData.data.name + ".lrc";
                 Loom.RunAsync(async () =>
                 {
+                    File.WriteAllText(lyricPath, apiData.data.lyric);
                     using (var web = new WebClient())
                     {
                         //web.DownloadProgressChanged += (s, e) =>
                         //{
                         //    Debug.LogWarning(e.ProgressPercentage + "%");
                         //};
-                        await web.DownloadFileTaskAsync(apiData.data.url, musicPath + "/" + apiData.data.name + ".mp3");
+                        await web.DownloadFileTaskAsync(apiData.data.url, musicFilePath);
                         Loom.QueueOnMainThread(() =>
                         {
-                            StartCoroutine(PlayMusic(musicPath + "/" + apiData.data.name + ".mp3"));
+                            StartCoroutine(PlayMusic(musicFilePath));
                         });
                     }
                 });
@@ -98,7 +126,17 @@ public class PanelMusic : MonoBehaviour
                 Debug.LogError(uwr.error);
                 yield break;
             }
-            AudioClip clip = DownloadHandlerAudioClip.GetContent(uwr);
+            AudioClip clip = null;
+            try
+            {
+                clip = DownloadHandlerAudioClip.GetContent(uwr);
+            }
+            catch(Exception e)
+            {
+                Log.Error(e.ToString());
+                StartCoroutine(RequestMusicUrl());
+                yield break;
+            }
             audioSource.clip = clip;
             audioSource.Play();
         }
@@ -107,14 +145,21 @@ public class PanelMusic : MonoBehaviour
     public class ApiData
     {
         public int code;
+        public string msg;
+        public string desc;
+        public string content;
+        public string updateTime;
         public Data data = new Data();
     }
 
     public class Data
     {
+        public int id;
         public string name;
+        public string singer;
+        public string duration;
+        public string cover;
         public string url;
-        public string picurl;
-        public string artistsname;
+        public string lyric;
     }
 }
